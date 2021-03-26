1ff9a2fb5f3c58d8ddd1ac182a44544e46a52271
Natalia Duran <o.nataliaduran@gmail.com>
2021-03-25 23:17:50 -0300

    Agrego menu desafio clase 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>E-commerce</h1>
+        <header className="nav">
+          <div className="nav__logo">Rollback</div>
+          <div className="nav__navbar">  
+            <div className="navbar__left">
+              <div>Categories</div>
+            </div>
+            <div className="navbar__right">
+              <ul className="navbar__userItems">
+                <li>Cart</li>
+                <li>Account</li>
+              </ul>
+            </div>
+          </div>
+        </header>
     </div>
   );
 }
