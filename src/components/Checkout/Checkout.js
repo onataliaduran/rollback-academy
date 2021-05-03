936e89e4d254d4c1f37245dc66a0065fa1cd4cad
Natalia Duran <o.nataliaduran@gmail.com>
2021-05-03 12:51:30 -0300

    fix

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,7 +1,7 @@
 import styles from "./Checkout.module.css";
 import { Link } from "react-router-dom";
 import { useState, useContext } from "react";
-import { CartContext, deleteAll } from "../../context/CartContext";
+import { CartContext } from "../../context/CartContext";
 import { submitOrder } from "../../data/data";
 
 const Checkout = ({ total }) => {
